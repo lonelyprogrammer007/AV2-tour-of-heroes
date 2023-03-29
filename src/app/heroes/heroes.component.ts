b2cfd292478c33a4c4ca03169e081d62100f5599
lugfigue <lugfigue@bancolombia.com.co>
2023-03-29 07:33:34 -0500

    c

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -9,31 +10,33 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
+  heroes!: Observable<Hero[]>;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
-    this.getHeroes();
+    // this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    // this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
-  add(name: string): void {
-    name = name.trim();
-    if (!name) {
-      return;
-    }
-    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
-      this.heroes.push(hero);
-    });
-  }
+  // add(name: string): void {
+  //   name = name.trim();
+  //   if (!name) {
+  //     return;
+  //   }
+  //   this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+  //     this.heroes.push(hero);
+  //   });
+  // }
 
-  delete(hero: Hero): void {
-    this.heroService.deleteHero(hero.id).subscribe((_) => {
-      this.heroes = this.heroes.filter((h) => h !== hero);
-    });
-  }
+  // delete(hero: Hero): void {
+  //   this.heroService.deleteHero(hero.id).subscribe((_) => {
+  //     this.heroes = this.heroes.filter((h) => h !== hero);
+  //   });
+  // }
 }
