42467415e105295c7f25a970b0ca91dabff5589d
lugfigue <lugfigue@bancolombia.com.co>
2023-04-04 10:39:47 -0500

    format code

--- src/app/infraestructure/driven-adapters/in-memory-hero-api/hero.service.spec.ts
+++ src/app/infraestructure/driven-adapters/in-memory-hero-api/hero.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { HeroService } from './hero-api-adapter.service';
+import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
   let service: HeroService;
